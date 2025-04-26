fb30cbe758669d53299f62b19539bbfefcad2fb4
Tofik Khan <tofik.khan@mkausa.org>
2025-04-26 15:00:09 -0700

    v4

--- packages/test/checkConnect/db.js
+++ packages/test/checkConnect/db.js
@@ -1,18 +1,16 @@
-const mongoose = require('mongoose')
+import { connect } from "mongoose";
 
 const dbURI = process.env.DB_URI;
 
 // establishing connection with the MongoDB database
 
-(
-    async () => {
-        try {
-            await mongoose.connect(dbURI);
-            // if connected successfully logging connected successfully
-            console.log("Connected to database")
-        } catch (error) {
-            // logging the error
-            console.log(error.message);
-        }
-    }
-)()
+(async () => {
+  try {
+    await connect(dbURI);
+    // if connected successfully logging connected successfully
+    console.log("Connected to database");
+  } catch (error) {
+    // logging the error
+    console.log(error.message);
+  }
+})();
