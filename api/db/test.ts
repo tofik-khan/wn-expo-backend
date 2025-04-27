4ee48b4471e12882eb798d3bbceb16c662bbdae3
Tofik Khan <tofik.khan@mkausa.org>
2025-04-27 01:37:14 -0700

    Add error catching to db calls

--- api/db/test.ts
+++ api/db/test.ts
@@ -1,15 +1,29 @@
 require("dotenv").config();
 import { MongoClient } from "mongodb";
 
+console.log("Connecting to DB");
 const client = new MongoClient(
   `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/`
 );
 
 export const dbTest = async (req, res) => {
-  const admins = await client
-    .db("wn-expo")
-    .collection("admins")
-    .find({})
-    .toArray();
-  res.send(JSON.stringify(admins));
+  console.log("Fetching Data");
+
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+
+    // Make the appropriate DB calls
+    const admins = await client
+      .db("wn-expo")
+      .collection("admins")
+      .find({})
+      .toArray();
+    res.send(JSON.stringify(admins));
+  } catch (e) {
+    console.error(e);
+  } finally {
+    // Close the connection to the MongoDB cluster
+    await client.close();
+  }
 };
