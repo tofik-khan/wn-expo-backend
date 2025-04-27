c3ae6de129b76f0acbab3e6462ed04a3f1f49998
Tofik Khan <tofik.khan@mkausa.org>
2025-04-27 00:55:09 -0700

    Add Env Variable Support

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,11 @@
+require('dotenv').config()
+
 import express from "express";
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.get("/hello", (req, res) => res.send(`Hello ${process.env.VERCEL_TEST} ${new Date().getTime()}`));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
