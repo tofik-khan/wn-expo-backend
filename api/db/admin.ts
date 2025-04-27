4129951be6239732d7270e848e60ada0a01444b8
Tofik Khan <tofik.khan@mkausa.org>
2025-04-27 12:01:29 -0700

    Add GetAdmins function and /admin route

--- api/db/admin.ts
+++ api/db/admin.ts
@@ -5,20 +5,24 @@ const client = new MongoClient(
   `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/`
 );
 
-export const dbTest = async (req, res) => {
-  console.log("Fetching Data");
+export const getAdmins = async (req, res) => {
 
   try {
     // Connect to the MongoDB cluster
     await client.connect();
 
-    // Make the appropriate DB calls
-    const collections = await client.db("wn-expo").listCollections().toArray();
-    res.send({ status: "success", data: collections });
+    const admins = await client
+      .db("wn-expo")
+      .collection("admins")
+      .find({})
+      .toArray()
+    res.send({status: "success", data: admins})
   } catch (e) {
     console.error(e);
+    res.status(500).send({status: "error", message: e.message})
   } finally {
-    // Close the connection to the MongoDB cluster
     await client.close();
   }
-};
+
+
+}
--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 import express from "express";
 import { dbTest } from "./db/test";
+import { getAdmins } from "./db/admin";
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
@@ -9,6 +10,8 @@ app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/hello", (req, res) => res.send(`Hello ${process.env.VERCEL_TEST} ${new Date().getTime()}`));
 
 app.get('/db/test', dbTest);
+
+app.get("/admin", getAdmins);
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
