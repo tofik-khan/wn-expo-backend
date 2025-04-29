a47f88d241850107647fe06562813b7dc0e0d689
Tofik Khan <tofik.khan@mkausa.org>
2025-04-28 18:41:24 -0700

    create get[Session|Presenters]Count routes

---
+++ api/db/presenter.ts
@@ -0,0 +1,24 @@
+require("dotenv").config();
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/`
+);
+
+export const getPresenterCount = async (req, res) => {
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+
+    const admins = await client
+      .db("wn-expo")
+      .collection("presenters")
+      .countDocuments({})
+    res.send({status: "success", data: admins})
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({status: "error", message: e.message})
+  } finally {
+    await client.close();
+  }
+}
--- api/index.ts
+++ api/index.ts
@@ -3,15 +3,23 @@ require('dotenv').config()
 import express from "express";
 import { dbTest } from "./db/test";
 import { getAdmins } from "./db/admin";
+import { getSessionsCount } from "./db/session";
+import { getPresenterCount } from "./db/presenter";
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.get("/hello", (req, res) => res.send(`Hello ${process.env.VERCEL_TEST} ${new Date().getTime()}`));
+app.get("/hello", (req, res) =>
+  res.send(`Hello ${process.env.VERCEL_TEST} ${new Date().getTime()}`)
+);
 
-app.get('/db/test', dbTest);
+app.get("/db/test", dbTest);
 
 app.get("/admin", getAdmins);
+
+app.get("/sessions/count", getSessionsCount);
+
+app.get("/presenters/count", getPresenterCount);
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
